118ff5c9ce1da86ed475e36a2689db02e84291fa
GoldenFighter <83505150+GoldenFighter@users.noreply.github.com>
2025-08-18 14:28:26 +0200

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,6 +10,8 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
+        isDone: a.boolean(),
+        owner: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
