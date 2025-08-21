ce74af629a4a480cc75d84884e4ee391d0e76f41
GoldenFighter <83505150+GoldenFighter@users.noreply.github.com>
2025-08-21 08:25:36 +0200

    Update route.ts

--- app/api/score/route.ts
+++ app/api/score/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { serverClient } from '@/src/lib/amplifyServerClient';
+import { serverClient } from '@/lib/amplifyServerClient';
 
 export async function POST(req: NextRequest) {
   const { prompt, context } = await req.json();
