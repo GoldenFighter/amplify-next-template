ba9fde0e2e2a174520c35bed7b975046f969a46d
Your Name <cmacleod5@me.com>
2025-09-10 08:53:02 +0200

    background change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 import "./app.css";
 import "./theme-globals.css";
 import ClientRoot from "./ClientRoot";
