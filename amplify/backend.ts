aa9e4c4cafacb6b192fcfc05db65ad77918e2d77
Your Name <cmacleod5@me.com>
2025-09-11 10:12:42 +0200

    backend changes

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
-import { storage } from './storage/resource';
-import { imageAnalysis } from './functions/imageAnalysis/resource';
+import { auth } from './auth/resource.js';
+import { data } from './data/resource.js';
+import { storage } from './storage/resource.js';
+import { imageAnalysis } from './functions/imageAnalysis/resource.js';
 import { exifExtraction } from './functions/exifExtraction/resource';
-import { rekognitionAnalysis } from './functions/rekognitionAnalysis/resource';
+// import { rekognitionAnalysis } from './functions/rekognitionAnalysis/resource.js';
 import { PolicyStatement, Effect } from 'aws-cdk-lib/aws-iam';
 import { LambdaDestination } from 'aws-cdk-lib/aws-s3-notifications';
 import { EventType } from 'aws-cdk-lib/aws-s3';
@@ -15,7 +15,7 @@ const backend = defineBackend({
   storage,
   imageAnalysis,
   exifExtraction,
-  rekognitionAnalysis,
+  // rekognitionAnalysis,
 });
 
 // Grant the function access to Bedrock
@@ -47,96 +47,96 @@ backend.imageAnalysis.resources.lambda.addToRolePolicy(
 );
 
 // Grant EXIF extraction function access to S3
-backend.exifExtraction.resources.lambda.addToRolePolicy(
-  new PolicyStatement({
-    effect: Effect.ALLOW,
-    actions: [
-      's3:GetObject',
-      's3:GetObjectVersion',
-    ],
-    resources: [
-      `${backend.storage.resources.bucket.bucketArn}/*`,
-    ],
-  })
-);
+// backend.exifExtraction.resources.lambda.addToRolePolicy(
+//   new PolicyStatement({
+//     effect: Effect.ALLOW,
+//     actions: [
+//       's3:GetObject',
+//       's3:GetObjectVersion',
+//     ],
+//     resources: [
+//       `${backend.storage.resources.bucket.bucketArn}/*`,
+//     ],
+//   })
+// );
 
-// Grant Rekognition analysis function access to S3 and Rekognition
-backend.rekognitionAnalysis.resources.lambda.addToRolePolicy(
-  new PolicyStatement({
-    effect: Effect.ALLOW,
-    actions: [
-      's3:GetObject',
-      's3:GetObjectVersion',
-    ],
-    resources: [
-      `${backend.storage.resources.bucket.bucketArn}/*`,
-    ],
-  })
-);
+// // Grant Rekognition analysis function access to S3 and Rekognition
+// backend.rekognitionAnalysis.resources.lambda.addToRolePolicy(
+//   new PolicyStatement({
+//     effect: Effect.ALLOW,
+//     actions: [
+//       's3:GetObject',
+//       's3:GetObjectVersion',
+//     ],
+//     resources: [
+//       `${backend.storage.resources.bucket.bucketArn}/*`,
+//     ],
+//   })
+// );
 
-backend.rekognitionAnalysis.resources.lambda.addToRolePolicy(
-  new PolicyStatement({
-    effect: Effect.ALLOW,
-    actions: [
-      'rekognition:DetectLabels',
-      'rekognition:DetectFaces',
-      'rekognition:DetectText',
-    ],
-    resources: ['*'],
-  })
-);
+// backend.rekognitionAnalysis.resources.lambda.addToRolePolicy(
+//   new PolicyStatement({
+//     effect: Effect.ALLOW,
+//     actions: [
+//       'rekognition:DetectLabels',
+//       'rekognition:DetectFaces',
+//       'rekognition:DetectText',
+//     ],
+//     resources: ['*'],
+//   })
+// );
 
-// Add S3 triggers for both functions
-backend.storage.resources.bucket.addEventNotification(
-  EventType.OBJECT_CREATED,
-  new LambdaDestination(backend.exifExtraction.resources.lambda),
-  {
-    prefix: 'contest-submissions/',
-    suffix: '.jpg',
-  }
-);
+// // Add S3 triggers for both functions
+// backend.storage.resources.bucket.addEventNotification(
+//   EventType.OBJECT_CREATED,
+//   new LambdaDestination(backend.exifExtraction.resources.lambda),
+//   {
+//     prefix: 'contest-submissions/',
+//     suffix: '.jpg',
+//   }
+// );
 
-backend.storage.resources.bucket.addEventNotification(
-  EventType.OBJECT_CREATED,
-  new LambdaDestination(backend.exifExtraction.resources.lambda),
-  {
-    prefix: 'contest-submissions/',
-    suffix: '.jpeg',
-  }
-);
+// backend.storage.resources.bucket.addEventNotification(
+//   EventType.OBJECT_CREATED,
+//   new LambdaDestination(backend.exifExtraction.resources.lambda),
+//   {
+//     prefix: 'contest-submissions/',
+//     suffix: '.jpeg',
+//   }
+// );
 
-backend.storage.resources.bucket.addEventNotification(
-  EventType.OBJECT_CREATED,
-  new LambdaDestination(backend.exifExtraction.resources.lambda),
-  {
-    prefix: 'contest-submissions/',
-    suffix: '.png',
-  }
-);
+// backend.storage.resources.bucket.addEventNotification(
+//   EventType.OBJECT_CREATED,
+//   new LambdaDestination(backend.exifExtraction.resources.lambda),
+//   {
+//     prefix: 'contest-submissions/',
+//     suffix: '.png',
+//   }
+// );
 
-backend.storage.resources.bucket.addEventNotification(
-  EventType.OBJECT_CREATED,
-  new LambdaDestination(backend.rekognitionAnalysis.resources.lambda),
-  {
-    prefix: 'contest-submissions/',
-    suffix: '.jpg',
-  }
-);
+// backend.storage.resources.bucket.addEventNotification(
+//   EventType.OBJECT_CREATED,
+//   new LambdaDestination(backend.rekognitionAnalysis.resources.lambda),
+//   {
+//     prefix: 'contest-submissions/',
+//     suffix: '.jpg',
+//   }
+// );
 
-backend.storage.resources.bucket.addEventNotification(
-  EventType.OBJECT_CREATED,
-  new LambdaDestination(backend.rekognitionAnalysis.resources.lambda),
-  {
-    prefix: 'contest-submissions/',
-    suffix: '.jpeg',
-  }
-);
+// backend.storage.resources.bucket.addEventNotification(
+//   EventType.OBJECT_CREATED,
+//   new LambdaDestination(backend.rekognitionAnalysis.resources.lambda),
+//   {
+//     prefix: 'contest-submissions/',
+//     suffix: '.jpeg',
+//   }
+// );
 
-backend.storage.resources.bucket.addEventNotification(
-  EventType.OBJECT_CREATED,
-  new LambdaDestination(backend.rekognitionAnalysis.resources.lambda),
-  {
-    prefix: 'contest-submissions/',
-    suffix: '.png',
-  }
-);
+// backend.storage.resources.bucket.addEventNotification(
+//   EventType.OBJECT_CREATED,
+//   new LambdaDestination(backend.rekognitionAnalysis.resources.lambda),
+//   {
+//     prefix: 'contest-submissions/',
+//     suffix: '.png',
+//   }
+// );
